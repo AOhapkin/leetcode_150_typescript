9e4519e0b3bb2312c8898c7275aeccb7c441b1f5
AOhapkin <alexeyyago@gmail.com>
2023-08-14 11:17:07 +0500

    roman to integer

---
+++ 013_roman_to_integer.ts
@@ -0,0 +1,31 @@
+function romanToInt(s: string): number {
+  const romanNumbers = {
+      I: 1,
+      IV: 4,
+      V: 5,
+      IX: 9,
+      X: 10,
+      XL: 40,
+      L: 50,
+      XC: 90,
+      C: 100,
+      CD: 400,
+      D: 500,
+      CM: 900,
+      M: 1000,
+  };
+
+  let result: number = 0;
+  let i: number = 0;
+  while(i < s.length) {
+      if ((i + 1) < s.length && romanNumbers[s.substring(i, i + 2)]) {
+          result += romanNumbers[s.substring(i, i + 2)];
+          i += 2;
+      } else {
+          result += romanNumbers[s[i]];
+          i++;
+      }
+  }
+
+  return result;
+};
