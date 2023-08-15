1a664c34cb9a18f859e1ad6e2cdd31234a55e913
AOhapkin <alexeyyago@gmail.com>
2023-08-15 12:45:02 +0500

    mergeTwoLists

---
+++ 021_mergeTwoLists.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const newHead: ListNode = new ListNode();
+    let pointer: ListNode = newHead;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            pointer.next = list1;
+            list1 = list1.next;
+        } else {
+            pointer.next = list2;
+            list2 = list2.next;
+        }
+        pointer = pointer.next;
+    }
+
+    pointer.next = list1 ? list1 : list2;
+
+    return newHead.next;
+};
