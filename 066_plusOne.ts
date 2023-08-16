86659bf338afb2287d6bada9463ac36fd1fc1156
AOhapkin <alexeyyago@gmail.com>
2023-08-16 19:51:47 +0500

    plusOne

---
+++ 066_plusOne.ts
@@ -0,0 +1,15 @@
+function plusOne(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            break;;
+        } else {
+            digits[i] = 0;
+        }
+    }
+
+    if (digits[0] === 0) {
+        digits = [1, ...digits];
+    }
+    return digits;
+};
