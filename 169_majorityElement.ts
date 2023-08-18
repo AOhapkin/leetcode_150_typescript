4448cdab49cf8fa6b397a3c152601c530f8f6d91
AOhapkin <alexeyyago@gmail.com>
2023-08-18 19:43:38 +0500

    majorityElement

---
+++ 169_majorityElement.ts
@@ -0,0 +1,17 @@
+function majorityElement(nums: number[]): number {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+      if (map.has(nums[i])) {
+          map.set(nums[i], map.get(nums[i]) + 1);
+      } else {
+          map.set(nums[i], 1);
+      }
+  }
+
+  for (let [key, value] of map) {
+      if (value > (nums.length / 2)) {
+          return key;
+      }
+  }
+};
