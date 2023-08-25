64b9f9d9dbd3c49988efe5b0d8f229a230baae88
AOhapkin <alexeyyago@gmail.com>
2023-08-25 18:16:47 +0500

    argumentsLength fix

--- 2703_argumentsLength.ts
+++ 2703_argumentsLength.ts
@@ -1,11 +1,5 @@
 function argumentsLength(...args: any[]): number {
-  let num: number = 0;
-  for (let arg in args) {
-      if (arg) {
-          num++
-      }
-  }
-  return num;
+  return [...args].length;
 };
 
 /**
